2580bcf8ed8af7ec4b0c4f21c87270833860b61a
Bhavin-Kareliya <bhavin.kareliya2017@gmail.com>
2021-10-29 12:09:34 +0530

    Fixed the details required for registration
    now only Email registration is enabled and
    also requires the username to be set at the same time

--- src/app/_models/RegisterEmailRequest.ts
+++ src/app/_models/RegisterEmailRequest.ts
@@ -4,10 +4,10 @@ export class RegisterEmailRequest {
     username?: string;
     public_key: string;
 
-    constructor(email: string, public_key: string){
+    constructor(email: string, username: string, public_key: string){
         this.email = email;
         this.phone_number = undefined;
-        this.username = undefined;
+        this.username = username;
         this.public_key = public_key;
     }
 }
