e862c00530432ac51e35ee043874a280389c3a19
Tushar Harsora <tusharharsora95@gmail.com>
2021-11-15 12:20:04 +0530

    Added Better constructor to initialize message

--- src/app/_models/Message.ts
+++ src/app/_models/Message.ts
@@ -7,13 +7,14 @@ export class Message {
     message_type_id : Number;
     message_at : Date;
 
-    constructor(){
-        this.id = -99;
-        this.sender_id = -99;
-        this.receiver_id = -1;
-        this.chat_relation_id = -999;
-        this.message = "uninitialized";
-        this.message_type_id = 1;
-        this.message_at = new Date();
+    constructor(id: Number = -99, sender_id: Number = -99, receiver_id: Number = -1, chat_relation_id: Number = -999,
+        message: string = "uninitialized", message_type_id: Number = 1, message_at: Date = new Date()){
+        this.id = id;
+        this.sender_id = sender_id;
+        this.receiver_id = receiver_id;
+        this.chat_relation_id = chat_relation_id;
+        this.message = message;
+        this.message_type_id = message_type_id;
+        this.message_at = message_at;
     }
 }
