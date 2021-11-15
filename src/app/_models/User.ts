1d4d3cf5cc1b80dd8d7aba98008db39948158411
Tushar Harsora <tusharharsora95@gmail.com>
2021-11-15 12:20:45 +0530

    Also store current userid when authenticating

--- src/app/_models/User.ts
+++ src/app/_models/User.ts
@@ -1,10 +1,12 @@
 export class User {
+    uid?: Number;
     email?: string;
     phone_number?: string;
     username?: string;
     token?: string;
 
     constructor(){
+        this.uid = -1;
         this.email = "example@example.com";
         this.phone_number = "9999999999";
         this.username = "uninitialized";
